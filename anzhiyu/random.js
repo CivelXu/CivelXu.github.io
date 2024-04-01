0b3b8e37e899b3e7bb2f63a125c6a5e89be1aca9
Xiwen Xu <xiwenxu@shein.com>
2024-04-01 17:44:14 +0800

    Site updated: 2024-04-01 17:44:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/apple/","2024/03/31/homekit/","2024/03/31/hello-world/","2024/03/31/ios-dev-note/","2024/03/31/life/"];function toRandomPost(){
+var posts=["2024/03/31/apple/","2024/03/31/homekit/","2024/03/31/hello-world/","2024/03/31/ios-dev-note/","2024/03/31/life/","2024/04/01/webp_convert/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
